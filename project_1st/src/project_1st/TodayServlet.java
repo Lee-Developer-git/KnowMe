b9aeb4d1ad89d2e052638d67cd07c0a95862651e
YunJehyeok <dbswpgur2@naver.com>
2020-01-06 12:59:01 +0900

    수정

--- project_1st/src/project_1st/TodayServlet.java
+++ project_1st/src/project_1st/TodayServlet.java
@@ -1,10 +1,6 @@
-package aboutme;
+package project_1st;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.text.SimpleDateFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class TodayServlet
-*/
+ */
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -28,23 +24,10 @@ public class TodayServlet extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	*/
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=utf-8");
-		PrintWriter out = response.getWriter();
-		
-		out.print("<html>");
-		out.print("<head><style>div { font-size: 40px; margin-top: 200px;text-align: center; } </style></head>");
-		out.print("<body>");
-		
-		out.print("<h1><a href=\"http://localhost:8080/aboutme/index.html\"><u>메인화면</u></a></h1>");
-		
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-		
-		out.print("<div>현재시간 : " + dtf.format(now) + "</div>");
-		
-		out.print("</body>");
-		out.print("</html>");
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+
 }
